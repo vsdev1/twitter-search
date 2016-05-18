f9b4a1db66ea2a7cfa954d7df849890044e4681a
u jann <uwe.janner@asideas.de>
2016-05-18 23:52:09 +0200

    fix service

--- src/app/twitter.service.ts
+++ src/app/twitter.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/map';
@@ -6,12 +6,24 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TwitterService {
 
-  constructor (private http: Http) {
+  constructor(private http:Http) {
 
   }
 
-  getArticles(searchTerm:string):Observable<any> {
-    return this.http.get(`http://content.guardianapis.com/search?api-key=test&q=${searchTerm}`).map(response => response.json());
+  getArticles(searchTerm:string):Observable<Array<Article>> {
+    return this.http.get(`http://content.guardianapis.com/search?api-key=test&q=${searchTerm}`).map(response => response.json().response.results.map(apiArticle=>new Article(apiArticle)));
   }
 
+}
+
+class Article {
+  id:string;
+  webTitle:string;
+  webUrl:string;
+
+  constructor(apiArticle:any) {
+    this.id = apiArticle.id;
+    this.webTitle = apiArticle.webTitle;
+    this.webUrl = apiArticle.webUrl;
+  }
 }

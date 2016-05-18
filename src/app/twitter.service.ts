2428324c39c50099ff2e7a114cb73f6cbfb07534
u jann <uwe.janner@asideas.de>
2016-05-18 23:39:26 +0200

    fix service

--- src/app/twitter.service.ts
+++ src/app/twitter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
-import {Observable} from 'rxjs/observable';
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class TwitterService {
@@ -10,7 +11,7 @@ export class TwitterService {
   }
 
   getArticles(searchTerm:string):Observable<any> {
-    return this.http.get('http://content.guardianapis.com/search?q=football&api-key=test').map(response => response.json());
+    return this.http.get(`http://content.guardianapis.com/search?api-key=test&q=${searchTerm}`).map(response => response.json());
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { TwitterSearchAppComponent, environment } from './app/';
+import { TwitterSearchAppComponent} from './app/twitter-search.component';
+import {environment} from './app/environment';
+import {HTTP_PROVIDERS} from '@angular/http'
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(TwitterSearchAppComponent);
+bootstrap(TwitterSearchAppComponent, [HTTP_PROVIDERS]);
 
